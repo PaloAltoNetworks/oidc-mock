e8cbd29b239911d889759a5df2736de2fab66403
Sibi Chakravarthy <sibi@aporeto.com>
2019-01-11 15:33:21 -0800

    Fix: Custom ciphers and versions

--- internal/entrypoint/entrypoint.go
+++ internal/entrypoint/entrypoint.go
@@ -1,6 +1,7 @@
 package entrypoint
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -50,8 +51,45 @@ func StartServer(cfg *Configuration) {
 
 	registerRoutes(r, cfg.ServerIP, cfg.ServerPort, cfg.PublicKeyPath, cfg.PrivateKeyPath, cfg.DevelopmentMode)
 
+	config := &tls.Config{
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_FALLBACK_SCSV,
+			tls.TLS_RSA_WITH_RC4_128_SHA,
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+		},
+		MinVersion: tls.VersionSSL30,
+		MaxVersion: tls.VersionTLS12,
+	}
+
+	server := &http.Server{
+		Addr:      cfg.ServerPort,
+		Handler:   r,
+		TLSConfig: config,
+	}
+
 	go func() {
-		if err := http.ListenAndServeTLS(cfg.ServerPort, ".data/server.crt", ".data/server.key", r); err != nil {
+		if err := server.ListenAndServeTLS(".data/server.crt", ".data/server.key"); err != nil {
 			zap.L().Fatal("Unable to start server",
 				zap.String("port", cfg.ServerPort),
 				zap.Error(err))
